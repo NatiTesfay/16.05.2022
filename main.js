741a92131e1bc96ac71cebd01410436eafe68782
NatiTesfay <netaneltes66@gmail.com>
2022-05-16 20:49:36 +0300

    j.s missions

---
+++ main.js
@@ -0,0 +1,258 @@
+// function myfunc(){
+//     document.write('nati');
+// }
+// myfunc();
+
+// function myfunc(){
+//     console.log('rehovot');
+// }
+// myfunc();
+
+// function myfunc(){
+//     alert('welcome');
+// }
+// myfunc();
+
+// function myfunc(){
+//     prompt('do you have a user');
+// }
+// myfunc();
+
+// function myfunc(){
+//     prompt('are you 18?');
+// }
+// myfunc();
+
+// function myfunc(){
+//     var x =0;
+//     if (x <= 18) {
+//         prompt('hey');
+//     }else{
+//         alert2('bye');
+//     }
+// }
+// myfunc();
+
+// function myfunc(){
+//     prompt('first name');
+// }
+// myfunc();
+
+
+// function myfunc(){
+//    var name ='nati';
+//    document.write('my name is'+''+ name);
+// }
+// myfunc();
+
+
+// function myfunc(){
+// for ( let i = 0; i < 100 ; i++){ //
+//     console.log(i);
+// }
+// }
+// myfunc();
+
+// function myfun(){
+// console.log('ISRAEL')
+// }
+// myfun();
+
+// function myfun(){
+//     var num =0;
+//     for (i=0; i<5; i++) {
+//         console.log(i);
+//     }
+// }
+// myfun();
+
+// function myfun(){
+//     var i = 5;
+//     var j = 5;
+//     var x = 5;
+//     var y = 5;
+//     var t =5;
+//     avg = i+j+x+y+t;
+//     console.log(avg/5)
+//     sum = i+j+x+y +t ;
+//     console.log(sum);
+//     }
+//     myfun();
+
+// function myfun(){
+//     var heigh = 10;
+//     var width = 5;
+//     console.log(width*heigh);
+// }
+// myfun();
+
+// function myfun(){
+//     var num1 =10;
+//     var num2 =2;
+//     console.log(num1=2,num2=10);
+// }
+// myfun();
+
+// Hoisting היא בעצם הנטייה של JavaScript להכריז על כל המשתנים בתחילת הבלוק של הקוד
+
+
+// function myfunc(){
+//     prompt('are you 18?');
+// }
+// myfunc();
+
+// function myfunc(){
+//     var x =0;
+//     if (x <= 18) {
+//         prompt('hey');
+//     }else{
+//         promot('bye');
+//     }
+// }
+// myfunc();
+
+// function myfunc(){
+//     prompt('first name');
+// }
+// myfunc();
+
+// function myfun(){
+//     num1 = 5;
+//     num2 = 8;
+//     console.log(num2)
+//     console.log(num1)
+// }
+// myfun();
+
+// function myfun(){
+//     num1 = 55;
+//     num2 = 89;
+//     console.log(num2)
+//     console.log(num1)
+// }
+// myfun();
+
+// function myfun(){
+//  var a1 = a;
+//  var a2 = 1;
+//  console.log(a2);
+//  console.log(a1);
+// }
+// myfun();
+
+
+// function myfunc(){
+// if ( a >= 1) {
+//    console.log(a);
+//    a++; 
+// }
+// }
+// myfunc();
+
+// function myfun(){
+//     var a = 0
+//     var b = 0;
+//     if (a == b) {
+//         console.log('true');
+//     }else if(b != a){
+//        console.log('false');
+//     }
+// }
+// myfun();
+
+
+// function myfunc(){
+//     var a =0;
+//     var b =0;
+//    if (a<b) {
+//        console.log(true);
+//    }
+//    else{
+//        console.log(false);
+//    }
+
+// }
+// myfunc();
+
+// function myfunc(){
+//   let a =0;
+//   let b =1;
+//   if (a != b || a>b) {
+//       console.log(true,false);
+//   }
+// }
+// myfunc();
+
+
+// function myfunc(){
+//     var num = 0;
+//     while (num == 4) {
+//         num++
+//         console.log(num)
+//     }
+// }
+// myfunc();
+
+// function myfunc(){
+//     var num = 4;
+//     var i = 0;
+//     if (i < 5 ) {
+//         console.log(num*3)
+//     }
+// }
+// myfunc();
+
+
+
+// function myfunc(){
+//     var name = 'avi';
+//     var name1 = 'aviel';
+//     var name2 = 'aviram';
+//     var i = 0;
+//     if (name2 < name1) {
+//         console(name ,'',20);
+//     } else if(name1 < name2) {
+//         console.log(name1 ,'',50)
+//     }else console(name2 ,'',16);
+// }
+// myfunc();
+
+// function myfuncs() {
+//     var i = 0;
+//     while (i<100) {
+//         console.log(i);
+//         i++;
+//     }
+// }
+// myfuncs();
+
+
+// function myfuncs() {
+//     var i = 0;
+//     while (i<51) {
+//         console.log(i); //to finish
+//         i++;
+//     }
+// }
+// myfuncs();
+
+// function myfunc(){
+//     var i =0;
+//     while (i<=50) {
+//         console.log(i+3);
+//         i++;
+//     }
+// }
+// myfunc();
+
+
+
+
+
+
+
+
+
+
+
+
